6f7bc332c85568f50a08991fe2292956fa2e4cee
Sebamon666 <sebastiansreyes@gmail.com>
2025-10-19 18:16:38 -0300

    Update xr.py

--- xr.py
+++ xr.py
@@ -208,6 +208,5 @@ def predict():
         "confidence": float(conf.item())
     }), 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=10000, debug=False)
